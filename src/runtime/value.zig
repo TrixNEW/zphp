ea275c9d09f630900ae44f0cd9f696c306737e6d
nvms <jon@pye.rs>
2026-08-20 12:58:42 -0400

    Revert "reuse cli generators"
    This reverts commit c4cc69872b7ee61c9d17ae6c1803da968f0eaac9.

--- src/runtime/value.zig
+++ src/runtime/value.zig
@@ -546,7 +546,6 @@ pub const Generator = struct {
     saved_handlers: [8]SavedHandler = undefined,
     delegate: ?DelegateState = null,
     pending_throw: ?Value = null,
-    pooled: bool = false,
 
     pub const SavedHandler = struct {
         catch_ip: usize,
